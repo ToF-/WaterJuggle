4ee5e52eca36ea12e4c0c62a3d02be1678417050
ToF <cthibaut@octo.com>
2019-01-02 22:56:58 +0100

    refactored tests

--- Specs.hs
+++ Specs.hs
@@ -1,51 +1,24 @@
 import Test.QuickCheck
 import WaterJuggle
 
-prop_initial _ = big initial == 0 && small initial == 0
+doCheck :: String -> (State -> Bool) -> IO ()
+doCheck s p = do
+    putStr (s ++ "\t : ")
+    quickCheck p
 
-prop_fillBig :: State -> Bool
-prop_fillBig state = big (fillBig state) == 5 && small (fillBig state) == small state
-
-prop_fillSmall :: State -> Bool
-prop_fillSmall state = small (fillSmall state) == 3 && big (fillSmall state) == big state
-
-prop_emptyBig :: State -> Bool
-prop_emptyBig state = big (emptyBig state) == 0 && small (emptyBig state) == small state
-
-prop_emptySmall :: State -> Bool
-prop_emptySmall state = small (emptySmall state) == 0 && big (emptySmall state) == big state
-
-prop_pourSmallInBig :: State -> Bool
-prop_pourSmallInBig s = let state = (abs (big s) `mod` 6, abs (small s) `mod` 4)
-                            state' = pourSmallInBig state
-    in big state <= 5 && small state <= 3
-    && if small state > 0 && big state < 5 then big state' > big state else True
-    && if big state' > big state then small state' < small state else True
-    && (big state' - big state) == (small state - small state')
-
-prop_pourBigInSmall :: State -> Bool
-prop_pourBigInSmall s = let state = (abs (big s) `mod` 6, abs (small s) `mod` 4)
-                            state' = pourBigInSmall state
-    in big state <= 5 && small state <= 3
-    && if big state > 0 && small state < 3 then small state' > small state else True
-    && if small state' > small state then big state' < big state else True
-
-prop_fillActions :: State -> Bool
-prop_fillActions _ = let state = actions [FillSmall,FillBig] initial
-    in small state == 3 && big state == 5
-
-prop_pourBigInSmallActions :: State -> Bool
-prop_pourBigInSmallActions _ = let state = actions [FillBig,PourBigInSmall] initial
-    in big state == 2 && small state == 3
-
-main = mapM_ quickCheck 
-    [prop_initial
-    ,prop_fillBig
-    ,prop_fillSmall
-    ,prop_emptyBig
-    ,prop_emptySmall
-    ,prop_pourSmallInBig
-    ,prop_pourBigInSmall
-    ,prop_fillActions
-    ,prop_pourBigInSmallActions
+main = mapM_ (uncurry doCheck)
+    [("big jug is initially empty", \_ -> big initial == 0)
+    ,("small jug is initially empty", \_ -> small initial == 0)
+    ,("fill small jug makes it 3 and leaves big jug unchanged", 
+        \state -> let state' = action FillSmallJug state
+        in small state' == 3 && big state' == big state)
+    ,("fill big jug makes it 5 and leaves small jug unchanged", 
+        \state -> let state' = action FillBigJug state
+        in big state' == 5 && small state' == small state)
+    ,("pour small into big, makes big no bigger than five",
+        \state -> let state' = action PourSmallIntoBig state
+        in big state' <= 5)
+    ,("pour non zero small into big, makes big bigger and small smaller ",
+        \state -> let state' = action PourSmallIntoBig state
+        in if small state > 0 && big state < 5 then big state' > big state && small state' < small state else True)
     ]
--- WaterJuggle.hs
+++ WaterJuggle.hs
@@ -1,34 +1,19 @@
 module WaterJuggle
 where
 
-type State = (Jug,Jug)
 type Jug = Integer
+type State = (Jug, Jug)
 
-data Action = FillSmall | FillBig | PourSmallInBig | PourBigInSmall | EmptyBig | EmptySmall
+data Action = FillSmallJug | FillBigJug | PourSmallIntoBig
     deriving (Eq, Show)
 
 initial = (0,0)
-solve = []
 
 big = fst
 small = snd
 
-fillBig (b,s) = (5,s)
-fillSmall (b,s) = (b,3)
+action :: Action -> State -> State
+action FillSmallJug (big,_) = (big,3)
+action FillBigJug (_,small) = (5,small)
+action PourSmallIntoBig (big, small) = let qty = min (5-big) small in (big+qty, small-qty)
 
-emptyBig (b,s) = (0,s)
-emptySmall (b,s) = (b,0)
-
-pourSmallInBig (b,s) = let q = min 5 (max s (5-b)) in (b+q, s-q)
-
-pourBigInSmall (b,s) = let q = min 3 (min b (3-s))  in (b-q,s+q)
-
-actions :: [Action] -> State -> State
-actions as state = foldl (\st a -> action a st) state as
-    where
-    action FillSmall = fillSmall 
-    action FillBig = fillBig
-    action PourSmallInBig = pourSmallInBig 
-    action PourBigInSmall = pourBigInSmall 
-    action EmptyBig = emptyBig
-    adtion EmptySmall = emptySmall
