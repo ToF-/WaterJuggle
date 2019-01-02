8177010faec92e05fbd83e137296d3cea4482128
ToF <cthibaut@octo.com>
2019-01-03 00:05:57 +0100

    all properties of jugs

--- Specs.hs
+++ Specs.hs
@@ -4,7 +4,13 @@ import WaterJuggle
 doCheck :: String -> (State -> Bool) -> IO ()
 doCheck s p = do
     putStr (s ++ "\t : ")
-    quickCheck p
+    quickCheck (p . forSomeCorrectState)
+
+invariant :: State -> Bool
+invariant (big,small) = big >= 0 && big <= 5 && small >= 0 && small <= 3
+
+forSomeCorrectState :: State -> State
+forSomeCorrectState (big,small) = ((abs big) `mod` 6, (abs small) `mod` 4)
 
 main = mapM_ (uncurry doCheck)
     [("big jug is initially empty", \_ -> big initial == 0)
@@ -15,10 +21,22 @@ main = mapM_ (uncurry doCheck)
     ,("fill big jug makes it 5 and leaves small jug unchanged", 
         \state -> let state' = action FillBigJug state
         in big state' == 5 && small state' == small state)
-    ,("pour small into big, makes big no bigger than five",
+    ,("pour small into big preserves invariant",
         \state -> let state' = action PourSmallIntoBig state
-        in big state' <= 5)
-    ,("pour non zero small into big, makes big bigger and small smaller ",
+        in invariant state' && totalWater state' == totalWater state)
+    ,("pour non zero small into big, makes big bigger and small smaller",
         \state -> let state' = action PourSmallIntoBig state
-        in if small state > 0 && big state < 5 then big state' > big state && small state' < small state else True)
+        in if small state > 0 && big state < 5 then big state' > big state && small state' < small state else state' == state && totalWater state' == totalWater state)
+    ,("pour big into small preserves invariant",
+        \state -> let state' = action PourBigIntoSmall state
+        in invariant state' && totalWater state' == totalWater state)
+    ,("pour non zero big into small, makes small bigger, and big smaller",
+        \state -> let state' = action PourBigIntoSmall state
+         in if big state > 0 && small state < 3 then small state' > small state && big state' < big state else state' == state && totalWater state' == totalWater state)
+    ,("empty small jug makes it zero",
+        \state -> let state' = action EmptySmallJug state 
+        in small state' == 0 && big state' == big state)
+    ,("empty big jug makes it zero",
+        \state -> let state' = action EmptyBigJug state 
+        in big state' == 0 && small state' == small state)
     ]
--- WaterJuggle.hs
+++ WaterJuggle.hs
@@ -4,7 +4,7 @@ where
 type Jug = Integer
 type State = (Jug, Jug)
 
-data Action = FillSmallJug | FillBigJug | PourSmallIntoBig
+data Action = FillSmallJug | FillBigJug | PourSmallIntoBig | PourBigIntoSmall | EmptySmallJug | EmptyBigJug
     deriving (Eq, Show)
 
 initial = (0,0)
@@ -12,8 +12,13 @@ initial = (0,0)
 big = fst
 small = snd
 
+totalWater (big,small) = big + small
 action :: Action -> State -> State
 action FillSmallJug (big,_) = (big,3)
 action FillBigJug (_,small) = (5,small)
 action PourSmallIntoBig (big, small) = let qty = min (5-big) small in (big+qty, small-qty)
+action PourBigIntoSmall (big, small) = let qty = min (3-small) big in (big-qty, small+qty)
+action EmptySmallJug (big, _) = (big, 0)
+action EmptyBigJug (_,small) = (0, small)
+
 
