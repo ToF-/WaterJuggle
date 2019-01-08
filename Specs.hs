6af6e37dec9b2f392ffd78359e89cb8fb4ea11eb
ToF- <cthibaut@octo.com>
2019-01-08 14:58:02 +0100

    state invariant

--- Specs.hs
+++ Specs.hs
@@ -1,7 +1,21 @@
 import Test.QuickCheck
 
+data State = State { big :: Integer, small :: Integer }
+    deriving (Show)
+
+instance Arbitrary State where
+    arbitrary = do 
+        b <- choose (0, 5)
+        s <- choose (0, 3)
+        return $ State b s
+
+prop_StateInvariant :: State -> Bool
+prop_StateInvariant st = big st >= 0 && big st <= 5 && small st >= 0 && small st <= 3
+
 main = do
     putStr "dummy check\t"
     quickCheck True
+    putStr "state invariant\t"
+    quickCheck prop_StateInvariant
 
     
