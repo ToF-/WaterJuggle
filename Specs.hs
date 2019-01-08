f256d0d6846d66736ebe44d8823010c7245920b7
ToF- <cthibaut@octo.com>
2019-01-08 15:02:21 +0100

    FillBig makes big = 5

--- Specs.hs
+++ Specs.hs
@@ -3,19 +3,30 @@ import Test.QuickCheck
 data State = State { big :: Integer, small :: Integer }
     deriving (Show)
 
+data Action = FillBig
+
 instance Arbitrary State where
     arbitrary = do 
         b <- choose (0, 5)
         s <- choose (0, 3)
         return $ State b s
 
+action :: Action -> State -> State
+action FillBig (State _ s) = State 5 s
+
 prop_StateInvariant :: State -> Bool
 prop_StateInvariant st = big st >= 0 && big st <= 5 && small st >= 0 && small st <= 3
+
+prop_FillBig :: State -> Bool
+prop_FillBig st = let st' = action FillBig st 
+                    in big st' == 5 && small st' == small st
 
 main = do
     putStr "dummy check\t"
     quickCheck True
     putStr "state invariant\t"
     quickCheck prop_StateInvariant
+    putStr "filling big makes big = 5"
+    quickCheck prop_FillBig
 
     
